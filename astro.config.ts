17a8f2ee5e000b595738734732595ba4857c0942
belandy <belandy202@gmail.com>
2023-08-20 17:27:04 +0900

    不要なコンポーネントを削除

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config'
-import preact from "@astrojs/preact"
 import unocss from 'unocss/astro'
 
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    preact(),
     unocss({injectReset: true}),
   ],
   experimental: {
