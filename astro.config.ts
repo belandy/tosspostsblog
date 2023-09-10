fa14ba0a3840eb1f0ed7b8a9f18a7b274cc4811b
Belandy <belandy202@gmail.com>
2023-09-10 21:47:53 +0900

    mdxを追加(現在未使用) assetのオプションを削除(デフォルトでオンになったため)

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 import unocss from 'unocss/astro'
-
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    unocss({injectReset: true}),
-  ],
-  experimental: {
-    assets: true,
-  },
+    unocss({
+      injectReset: true
+    }),
+    mdx(),
+  ]
 });
