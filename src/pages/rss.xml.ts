93da3182f593a6ee74f6bbef597c457d172d94ac
Belandy <belandy202@gmail.com>
2023-09-10 21:46:41 +0900

    rssをコンテンツコレクションに対応

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,11 +1,18 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
-export async function get() {
+export async function GET() {
+  const posts = await getCollection('blog');
   return rss({
     title: 'TossPostsBlog',
-    description: 'なんでもいい',
+    description: 'なにをしてもいいブログ',
     site: 'https://tosspostsblog.netlify.app',
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+    })),
     customData: `<language>ja-jp</language>`,
   });
 }
